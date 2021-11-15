0c8002442539f33cc0c9f87c0a8c8a5c20dd1999
Andrew52F <andrew52ferrari@gmail.com>
2021-11-15 15:56:55 +0300

    Add brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+import { greetings, name } from './../src/cli.js';
+import gameRules from './../index.js';
+import { rule, questions, corrects } from './../src/games/progression-game.js';
+
+greetings();
+gameRules(questions, corrects, rule, name);
---
+++ src/games/progression-game.js
@@ -0,0 +1,30 @@
+import getRandomInt from './../../random-num.js';
+const rule = 'What number is missing in the progression?';
+const questions = [];
+const corrects = [];
+for (let i = 0; i < 3; i += 1) {
+  const row = [];
+  const rowLength = 5 + getRandomInt(5);
+  const differentiator = 1 + getRandomInt(4);
+
+  for (let j = 0; j < rowLength; j += 1) {
+    if (row.length === 0) {
+      row.push(getRandomInt(100));
+    } else {
+      row.push(row[j - 1] + differentiator);
+    }
+  }
+
+  const missingItemID = getRandomInt(rowLength);
+  corrects.push(String(row[missingItemID]));
+  row[missingItemID] = '..';
+  let rowString = '';
+  for (let j = 0; j < rowLength; j += 1) {
+    rowString += row[j] + ' ';
+  }
+  questions.push(rowString);
+}
+
+export {
+  rule, questions, corrects
+};
