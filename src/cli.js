08f3e3a9197458ff84d641b43ed8e120a95d9bbe
Andrew52F <81577250+Andrew52F@users.noreply.github.com>
2021-11-17 13:55:41 +0300

    Fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const name = '';
+let name = '';
 const greetings = () => {
   console.log('Welcome to the Brain Games!');
   name = readlineSync.question('May I have your name? ');
@@ -8,4 +8,4 @@ const greetings = () => {
   return name;
 };
 
-export { name, greetings };
+export { greetings };
