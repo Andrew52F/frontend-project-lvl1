71038814966bc53592addf09f382ff3b9a7dcbf2
Andrew52F <81577250+Andrew52F@users.noreply.github.com>
2021-11-26 17:28:11 +0300

    Fix code

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -5,17 +5,20 @@ const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
   let mark = true;
+  if (number < 2) {
+    return false;
+  }
   for (let j = 2; j <= Math.sqrt(number); j += 1) {
     if (number % j === 0) {
       mark = false;
     }
   }
-  return (mark) ? 'yes' : 'no';
+  return mark;
 };
 
 const getQuestionsAndCorrects = () => {
   const number = getRandomInt(1, 100);
-  const correct = isPrime(number);
+  const correct = isPrime(number) ? 'yes' : 'no';
   return [number, correct];
 };
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import readlineSync from 'readline-sync';
-import greetings from './cli.js';
 
 const roundsCount = 3;
 
 const gameRules = (rule, getQuestionsAndCorrects) => {
-  const name = greetings();
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 
   console.log(rule);
   for (let i = 0; i < roundsCount; i += 1) {
-    const questionsAndCorrects = getQuestionsAndCorrects();
-    console.log(`Question: ${questionsAndCorrects[0]}`);
+    const [qestion, correct] = getQuestionsAndCorrects();
+    console.log(`Question: ${qestion}`);
 
     const answer = readlineSync.question('Your answer: ');
 
-    if (questionsAndCorrects[1] !== answer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${questionsAndCorrects[1]}'.`);
+    if (correct !== answer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
